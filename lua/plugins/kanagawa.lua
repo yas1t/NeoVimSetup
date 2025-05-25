b71026b151a3726d530a662d49a2299f5fda6a4f
yas <yasalwaily20@gmail.com>
2025-05-24 18:18:16 -0700

    original

---
+++ lua/plugins/kanagawa.lua
@@ -0,0 +1,16 @@
+return {
+
+	"rebelot/kanagawa.nvim",
+	config = function()
+		require("kanagawa").setup({
+
+			compile = true,
+			transparent = true,
+		})
+		vim.cmd("colorscheme kanagawa")
+	end,
+
+	build = function()
+		vim.cmd("KanagawaCompile")
+	end,
+}
